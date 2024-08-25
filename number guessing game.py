cf9ed0da73a18d3674dc0015d877eb398603de42
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-25 18:44:00 +0330

    Congratulation guys! This time I really made it

--- number guessing game.py
+++ number guessing game.py
@@ -10,24 +10,22 @@ max_num_guess = int(math.log2(upper_bound - lower_bound +1))
 def number_guessing_game():
     print(f"You've got {max_num_guess} guesses to try")
     guess_number = int(input("Please enter your guess number: "))
-    count_guess = 0
+    count_guess = 1
     if guess_number == random_number:
-        return "Congratulations!\nTotal Number of Guesses = 1"
-    else:
-        while count_guess < (max_num_guess - 1):
-            if guess_number == random_number:
-                count_guess += 1
-                return f"Congratulations!\nTotal Number of Guesses = {count_guess}"
-            elif guess_number < random_number:
-                print("Try Again! You guessed too small.")
-                count_guess += 1
-                print(f"You've still got {max_num_guess - count_guess} guess(es) to try")
-                guess_number = int(input('guess again: '))
-            else:
-                print("Try Again! You guessed too high.")
-                count_guess += 1
-                print(f"You've still got {max_num_guess - count_guess} guess(es) to try")
-                guess_number = int(input('guess again: '))
-        return f"Better Luck Next Time!\nTotal Number of Guesses = {count_guess}"
+        return "Congratulations!\nTotal Number of Guesses = 1" 
+    while count_guess < max_num_guess:
+        if guess_number == random_number:
+            return f"Congratulations!\nTotal Number of Guesses = {count_guess}"
+        elif guess_number < random_number:
+            print(f"Try Again! You guessed too small.\nYou've still got {max_num_guess - count_guess} guess(es) to try")
+            guess_number = int(input('guess again: '))
+            count_guess += 1
+        else:
+            print(f"Try Again! You guessed too high.\nYou've still got {max_num_guess - count_guess} guess(es) to try")
+            guess_number = int(input('guess again: '))
+            count_guess += 1
+    if guess_number == random_number:
+        return f"Congratulations!\nTotal Number of Guesses = {count_guess}"
+    return f"Better Luck Next Time!\nTotal Number of Guesses = {count_guess}\nThe random number was: {random_number}"
 
 print(number_guessing_game())
