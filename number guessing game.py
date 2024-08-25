df8fbefe5e0ed78c6ff55ea49d0619661e723ef9
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-25 16:43:59 +0330

    Codes related to the endpoints of the range and the guess number

--- number guessing game.py
+++ number guessing game.py
@@ -1,1 +1,7 @@
 from random import randint
+
+starting_endpoint = int(input(Please enter the starting endpoint of your range: ))
+ending_endpoint = int(input(Please enter the ending endpoint of your range: ))
+
+random_number = randint(starting_endpoint, ending_endpoint)
+
