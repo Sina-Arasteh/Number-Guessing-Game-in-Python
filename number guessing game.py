2cb40942ba7f37b622843f838074f2ee6a6c34e5
Sina Arasteh <sinna.arasteh@gmail.com>
2024-08-25 17:22:11 +0330

    Add some features

--- number guessing game.py
+++ number guessing game.py
@@ -1,7 +1,22 @@
-from random import randint
+import random
+import math
 
-starting_endpoint = int(input(Please enter the starting endpoint of your range: ))
-ending_endpoint = int(input(Please enter the ending endpoint of your range: ))
+lower_bound = int(input("Please enter the starting endpoint of your range: "))
+upper_bound = int(input("Please enter the ending endpoint of your range: "))
 
-random_number = randint(starting_endpoint, ending_endpoint)
+random_number = random.randint(lower_bound, upper_bound)
+minimum_num_guess = math.log2(upper_bound - lower_bound +1)
 
+guess_number = int(input("Please enter your guess number: "))
+count_guess = 1
+while guess_number != random_number:
+    if guess_number < random_number:
+        print("Try Again! You guessed too small.")
+        count_guess += 1
+        guess_number = int(input('guess again: '))
+    else:
+        print("Try Again! You guessed too high.")
+        count_guess += 1
+        guess_number = int(input('guess again: '))
+
+return f"Total Number of Guesses = {count_guess}"
